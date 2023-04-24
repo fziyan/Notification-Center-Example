c9213eed77dca317150c60f24218148a30c57b6c
Fatihan Ziyan <ziyanfatihan@gmail.com>
2023-04-24 23:50:02 +0300

    Initial Commit

---
+++ Notification Center Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notification Center Example
+//
+//  Created by Fatihan Ziyan on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Notification Center ExampleUITests/Notification_Center_ExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Notification_Center_ExampleUITestsLaunchTests.swift
+//  Notification Center ExampleUITests
+//
+//  Created by Fatihan Ziyan on 24.04.2023.
+//
+
+import XCTest
+
+final class Notification_Center_ExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
